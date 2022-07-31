456080bb06c30e4014ac40f0c4f11ab0da7863c0
0b5vr <0b5vr@0b5vr.com>
2022-07-31 23:36:54 +0900

    size: randomTexture is okay to be GL_R8

--- src/randomTexture.ts
+++ src/randomTexture.ts
@@ -1,20 +1,20 @@
-import { GL_LINEAR, GL_RGBA, GL_RGBA8, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_TEXTURE_MIN_FILTER, GL_UNSIGNED_BYTE } from './gl-constants';
+import { GL_LINEAR, GL_R8, GL_RED, GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_TEXTURE_MIN_FILTER, GL_UNSIGNED_BYTE } from './gl-constants';
 import { gl } from './canvas';
 
 export const randomTexture = gl.createTexture()!;
 
-const source = new Uint8Array( 262144 ).map( () => 256.0 * Math.random() );
+const source = new Uint8Array( 65536 ).map( () => 256.0 * Math.random() );
 
 gl.bindTexture( GL_TEXTURE_2D, randomTexture );
 
 gl.texImage2D(
   GL_TEXTURE_2D, // target
   0, // level
-  GL_RGBA8, // internalformat
+  GL_R8, // internalformat
   256, // width
   256, // height
   0, // border
-  GL_RGBA, // format
+  GL_RED, // format
   GL_UNSIGNED_BYTE, // type
   source, // pixels
 );
