d0fce1520e13b20f1529ac7535e0336ce9fe1f76
0b5vr <0b5vr@0b5vr.com>
2022-07-31 11:44:07 +0900

    fix: NaN guard BPM input

--- src/ui.ts
+++ src/ui.ts
@@ -13,4 +13,4 @@ export let bps = 190.0 / 60.0;
 const ass = document.querySelectorAll( 'a' );
 inputGain.oninput = ( () => ass[ 0 ].innerText = parseFloat( inputGain.value ).toFixed( 3 ) );
 inputKnob.oninput = ( () => ass[ 1 ].innerText = parseFloat( inputKnob.value ).toFixed( 3 ) );
-inputBPM.oninput = ( () => bps = parseFloat( inputBPM.value ) / 60.0 );
+inputBPM.oninput = ( () => bps = ( parseFloat( inputBPM.value ) || 190.0 ) / 60.0 );
