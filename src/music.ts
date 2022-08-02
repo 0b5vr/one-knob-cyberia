cb9ce1f2e86c56d70bf099dcd8c38ef1157874ff
0b5vr <0b5vr@0b5vr.com>
2022-08-03 05:24:57 +0900

    size: crush more (3/3)
    omit unnecessary early return on `updateMusic()`

--- src/music.ts
+++ src/music.ts
@@ -14,44 +14,42 @@ let fourBar = 0;
 let bufferWriteBlocks = 0;
 
 export function updateMusic(): void {
-  if ( !bufferReaderNode ) { return; }
+  if ( bufferReaderNode ) {
+    bufferReaderNode.port.postMessage( isPlaying );
 
-  bufferReaderNode.port.postMessage( isPlaying );
+    // if ( !isPlaying ) { return; }
 
-  // -- early abort? -------------------------------------------------------------------------------
-  if ( !isPlaying ) { return; }
+    // -- choose a right write block ---------------------------------------------------------------
+    const blockAhead = bufferWriteBlocks - readBlocks;
 
-  // -- choose a right write block -----------------------------------------------------------------
-  const blockAhead = bufferWriteBlocks - readBlocks;
+    // we don't have to render this time
+    if ( blockAhead > LATENCY_BLOCKS ) {
+      return;
+    }
+    // we're very behind
+    if ( blockAhead < 0 ) {
+      bufferWriteBlocks = (
+        ~~( readBlocks / BLOCKS_PER_RENDER ) + 1
+      ) * BLOCKS_PER_RENDER;
+    }
 
-  // we don't have to render this time
-  if ( blockAhead > LATENCY_BLOCKS ) {
-    return;
+    // -- render -----------------------------------------------------------------------------------
+    render( fourBar );
+
+    const offset = bufferWriteBlocks * BLOCK_SIZE % BUFFER_FRAMES_PER_CHANNEL;
+
+    bufferReaderNode.port.postMessage( [
+      dstArray0.subarray( 0, FRAMES_PER_RENDER ),
+      offset,
+    ] );
+
+    bufferReaderNode.port.postMessage( [
+      dstArray1.subarray( 0, FRAMES_PER_RENDER ),
+      offset + BUFFER_FRAMES_PER_CHANNEL,
+    ] );
+
+    // -- update write blocks and position ---------------------------------------------------------
+    fourBar = ( fourBar + BLOCKS_PER_RENDER * BLOCK_SIZE / sampleRate * bps ) % 16.0;
+    bufferWriteBlocks += BLOCKS_PER_RENDER;
   }
-
-  // we're very behind
-  if ( blockAhead < 0 ) {
-    bufferWriteBlocks = (
-      ~~( readBlocks / BLOCKS_PER_RENDER ) + 1
-    ) * BLOCKS_PER_RENDER;
-  }
-
-  // -- render -------------------------------------------------------------------------------------
-  render( fourBar );
-
-  const offset = bufferWriteBlocks * BLOCK_SIZE % BUFFER_FRAMES_PER_CHANNEL;
-
-  bufferReaderNode.port.postMessage( [
-    dstArray0.subarray( 0, FRAMES_PER_RENDER ),
-    offset,
-  ] );
-
-  bufferReaderNode.port.postMessage( [
-    dstArray1.subarray( 0, FRAMES_PER_RENDER ),
-    offset + BUFFER_FRAMES_PER_CHANNEL,
-  ] );
-
-  // -- update write blocks and position -----------------------------------------------------------
-  fourBar = ( fourBar + BLOCKS_PER_RENDER * BLOCK_SIZE / sampleRate * bps ) % 16.0;
-  bufferWriteBlocks += BLOCKS_PER_RENDER;
 }
